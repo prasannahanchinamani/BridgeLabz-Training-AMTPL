5509b25b6bc9a4c92cb6d57a3d6cad28479508eb
Prasanna Hanchinamani <prasannahanchinamani.15@gmail.com>
2025-10-02 19:38:35 +0530

    pojo->start Spring IOC -> Bean Creation -> Dependency Injection->PostConstruct->Ready to use -> PreDestroy->Bean Destroy ->stop

--- conceptwise/src/main/java/com/conceptswise/conceptwise/ConceptwiseApplication.java
+++ conceptwise/src/main/java/com/conceptswise/conceptwise/ConceptwiseApplication.java
@@ -4,16 +4,14 @@ import com.conceptswise.conceptwise.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ConceptwiseApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(ConceptwiseApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ConceptwiseApplication.class, args);
         System.out.println("Started the ConceptWise.........");
         System.out.println("ApplicationContext hashcode: " + context.hashCode());
-//        User user = context.getBean(User.class);
-//        user.user_method();
-//        System.out.println("user Object " + user.hashCode());
-
+        context.close();
     }
 }
--- conceptwise/src/main/java/com/conceptswise/conceptwise/entities/User.java
+++ conceptwise/src/main/java/com/conceptswise/conceptwise/entities/User.java
@@ -1,9 +1,12 @@
 package com.conceptswise.conceptwise.entities;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
@@ -14,12 +17,21 @@ public class User {
     String user_name;
     Long Contact_number;
 
+//    @Autowired
     public User(String user_name, Long contact_number) {
+        System.out.println("user constructor");
         this.user_name = user_name;
         Contact_number = contact_number;
     }
 
+     @PostConstruct
     public void user_method() {
+         System.out.println("IntialisationBean : Interface after Bean is Created");
         System.out.println("From user method: " + this.hashCode());
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("DisposableBean before bean Destroy:");
+        System.out.println("From user Method "+this.hashCode());
     }
 }
