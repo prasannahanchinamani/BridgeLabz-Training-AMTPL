079c37711ecb3b2d1727dba71fe34ae4a923e114
Prasanna <prasannahanchinamani.15@gmail.com>
2025-11-12 20:29:25 +0530

    Loops and Conditional statements

---
+++ LoopsAndCondition/FirstnonRepeating.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionAndLoops.LoopsAndCondition
+{
+    internal class FirstnonRepeating
+    {
+        public static char? firstnonRepeating(String st)
+        {
+            for(int i = 0; i < st.Length; i++)
+            {
+                bool isFound = true;
+                for (int j = 0; j < st.Length; j++)
+                {
+                    if (i != j && st[j] == st[i])
+                    {
+                        isFound = false;
+                        break;
+                    }
+                }
+                    if (isFound)
+                        return st[i];
+                
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ConditionAndLoops.Loops;
+using ConditionAndLoops.LoopsAndCondition;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a string : ");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseString.Reverse(input);
+        Console.WriteLine("Reversed string: " + reversed);
+
+        bool isPalindrome = PalindromeCheck.isPalindrome(input);
+        Console.WriteLine("Is Palindrome: " + isPalindrome);
+        Console.WriteLine(" Anagram Checking :");
+        Console.WriteLine("String input 1");
+        String input1 = Console.ReadLine();
+        Console.WriteLine("String input 2");
+        String input2 = Console.ReadLine();
+
+        Anagram.isAnagram(input1, input2);
+        Console.WriteLine("Count Vowel And Consonant");
+        CountVowelConsonents.countVowelConsonentsInString(input);
+        LongestSubstringWithoutRepeating.longestSubstring(input);
+        String res = CompressString.compreesedString(input);
+        Console.WriteLine(res);
+
+        char? result = FirstnonRepeating.firstnonRepeating("aabbcc");
+        if (result.HasValue)
+            Console.WriteLine("First unique: " + result);
+        else
+            Console.WriteLine("No unique character found.");
+
+        Console.Write("Is String Rotation of Another:"+ StringRotation.isRotationOfAnother("waterbottle", "erbottlewat"));
+    }
+}
