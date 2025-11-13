4bfba696a4307b9099c26f753f0258dd98236c72
Prasanna <prasannahanchinamani.15@gmail.com>
2025-11-13 12:06:23 +0530

    Worked on Arrays Promblems

---
+++ Arrays/RotateArrayByK.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics.Arrays
+{
+    internal class RotateArrayByK
+    {
+        public static int[] rotateArray(int[] nums, int k)
+        {
+            int n = nums.Length;
+            k = k % n;
+            reverse(nums, 0, n- 1);
+            reverse(nums, 0,k-1);
+            reverse(nums, k, n - 1);
+
+            return nums;
+
+        }
+        public static void reverse(int[] nums, int start, int end)
+        {
+          
+            while (start < end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
