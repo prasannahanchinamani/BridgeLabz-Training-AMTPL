6b12cf883bf140e8fc28c9521549098bb4f56c4a
Prasanna H <prasannahanchinamani@gmail.com>
2025-11-24 15:21:30 +0530

    extra

---
+++ BridgeLabz-Training-AMTPL/BridgeLabz-Training-AMTPL/Cl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training_AMTPL
+{
+    class Cl
+    {
+    Console.WriteLine("Hello");
+    }
+}
