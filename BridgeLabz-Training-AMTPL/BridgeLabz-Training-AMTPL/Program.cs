1ae365d46e488328e96a8cd204b0fdaef18b6cad
Prasanna H <prasannahanchinamani@gmail.com>
2025-11-24 10:35:17 +0530

    adding struct and delegate examples

---
+++ BridgeLabz-Training-AMTPL/BridgeLabz-Training-AMTPL/Program.cs
@@ -0,0 +1,24 @@
+﻿using BridgeLabz_Training_AMTPL;
+using static BridgeLabz_Training_AMTPL.DelegateExample;
+using static BridgeLabz_Training_AMTPL.DelegateExample;
+using static BridgeLabz_Training_AMTPL.StruckExample;
+
+Console.WriteLine("Welcome to Address Book ");
+int i = 10;
+int j = i;
+
+//struct example
+position pos = new position(10, 20);
+pos.display();
+
+//delegate example - used for user design patterns
+DelegateExample del=new DelegateExample();
+exampleDelegate exampleDel = new exampleDelegate(del.DisplayMeassage);
+   exampleDel("Hello from delegate");
+
+//multicast delegate
+
+exampleDelegate1 exampleDel1 = AddNumbers;
+exampleDel1 += AddThreeNumbers;
+int result = exampleDel1(10, 20);
+Console.WriteLine("Result from multicast delegate: " + result);                 
