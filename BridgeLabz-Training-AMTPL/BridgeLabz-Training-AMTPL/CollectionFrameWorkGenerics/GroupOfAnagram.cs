5179f00481b27bf652229e25d19abba92f946626
Prasanna <prasannahanchinamani.15@gmail.com>
2025-11-28 11:08:18 +0530

    Added regukar expression and genreics

---
+++ BridgeLabz-Training-AMTPL/BridgeLabz-Training-AMTPL/CollectionFrameWorkGenerics/GroupOfAnagram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training_AMTPL.CollectionFrameWorkGenerics
+{
+    internal class GroupOfAnagram
+    {
+        public static Dictionary<string,List<string>> GroupOfAnanagram(List<String> words)
+        {
+            var result=new Dictionary<string, List<string>>();
+            foreach(string word in words)
+            {
+                char[] ch = word.ToCharArray();
+                Array.Sort(ch);
+                string sortedWord = new string (ch);
+                if (result.ContainsKey(sortedWord)){
+                    result[sortedWord].Add(word);
+                }
+                else
+                {
+                    result[sortedWord] = new List<string>() { word };
+                }
+            }
+            return result;
+        
+        }
+        public static void Main(string[] args)
+        {
+            List<string> words = new List<string>()
+            {
+                "eat", "tea", "tan", "ate", "nat", "bat"
+            };
+            var groupedAnagrams = GroupOfAnanagram(words);
+            foreach(var kvp in groupedAnagrams)
+            {
+                Console.WriteLine("Anagrams for key " + kvp.Key + ": " + string.Join(", ", kvp.Value));
+            }
+        }   
+    }
+}
---
+++ BridgeLabz-Training-AMTPL/BridgeLabz-Training-AMTPL/CollectionFrameWorkGenerics/LinkedListExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training_AMTPL.CollectionFrameWorkGenerics
+{
+    internal class LinkedListExample
+    {
+        public static void LinkedListDemo()
+        {
+            LinkedList<string>list=new LinkedList<string>();
+            list.AddFirst("hi");
+            list.AddLast("hello");
+            list.AddFirst("GoodMorning");
+            list.AddLast("Welcome");
+            foreach(string str in list)
+            {
+                Console.WriteLine(str);
+            }
+            list.Remove("hi");
+            list.RemoveFirst();
+            foreach (string str in list)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            LinkedListDemo();
+        }
+    }
+}
