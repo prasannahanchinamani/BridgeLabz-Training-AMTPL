a4481ac6de6b99b429deffcbe83c48bc139e9d32
Prasanna <prasannahanchinamani.15@gmail.com>
2025-11-28 11:21:49 +0530

    Delete Desktop/Spring/springcore directory

--- Desktop/Spring/springcore/src/main/java/net/springcores/employeeManagement/Configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package net.springcores.employeeManagement.Configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("net.springcores.employeeManagement")
-public class AppConfig {
-}
