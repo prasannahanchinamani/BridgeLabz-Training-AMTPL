f8a2144599a552daafdc7171aef127f6aedee6aa
Prasanna H <prasannahanchinamani@gmail.com>
2025-12-01 14:54:33 +0530

    add files

---
+++ Desktop/DOTNET/BridgeLabz-Training-AMTPL/LamdaExpression/ClosureExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BridgeLabz_Training_AMTPL.LambdaExpression
+
+//namespace BridgeLabz_Training_AMTPL.LamdaExpression
+{
+    internal class ClosureExample
+    {
+        int fact = 10;
+        Func<int, int> square;
+
+        public ClosureExample()
+        {
+            square = x => x * fact;
+        }
+
+        public int RunSquare(int x) => square(x); 
+
+        public static void Main(string[] args)
+        {
+            ClosureExample obj = new();
+            int res = obj.RunSquare(5);   
+            Console.WriteLine($"Closure result: {res}");
+        }
+    }
+}
